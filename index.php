e4e1bc73cdf26095231c33c41ced832093d4b045
Rens Werink <Rens Werink@localhost>
2015-05-10 23:12:53 +0200

    added bootstrap(2)

--- index.php
+++ index.php
@@ -10,14 +10,16 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+// echo "Connected successfully";
 ?>
 
 <html>
 	<head>
-	Wanneer komt het loon?
+		<title>Wanneer komt het loon?</title>
+		<link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+		<script type="text/javascript" src="/bootstrap/js/bootstrap.min.js" >
 	</head>
 	<body>
-	<h1>Wanneer komt het loon?</h1>
+		<h1>Wanneer komt het loon?</h1>
 	</body>
 </html>
